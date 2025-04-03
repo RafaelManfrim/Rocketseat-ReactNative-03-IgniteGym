520761cc7b8ffe448ab41c6265e819533a155d11
manfrim <rafaelmanfrim2004@gmail.com>
2025-04-02 23:29:59 -0300

    Criando componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
-import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <GluestackUIProvider config={config}>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "#202024" }}>
-        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-        {fontsLoaded ? <Text>Home</Text> : <View></View>}
-      </View>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoaded ? (
+        <Center flex={1} bg="$gray700">
+          <Text>Home</Text>
+        </Center>
+      ) : (
+        <Loading />
+      )}
     </GluestackUIProvider>
   );
 }
