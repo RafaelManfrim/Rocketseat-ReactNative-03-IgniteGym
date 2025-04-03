c48991ce3e0ec0ebfd374fa3206cb04273c7ec07
manfrim <rafaelmanfrim2004@gmail.com>
2025-04-02 23:40:13 -0300

    Iniciando desenvolvimento da tela de SignIn

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
-import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -11,9 +12,7 @@ export default function App() {
     <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
       {fontsLoaded ? (
-        <Center flex={1} bg="$gray700">
-          <Text>Home</Text>
-        </Center>
+        <SignIn />
       ) : (
         <Loading />
       )}
